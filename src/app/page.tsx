f7d685f8f70958fd3fd5345becc437de4b98d018
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-03-26 15:32:45 +0700

    feat(auth): Integrate jwt-decode for authentication cookie management and enhance post utility for header handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <></>
+import getMe from "@/actions/get-me"
+
+export default async function Home() {
+  const me = await getMe()
+  return <>
+    <h1>User: {me?.email}</h1>
+  </>
 }
--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -1,13 +1,27 @@
-export const post = async (path: string, formData: FormData) => {
+import { cookies } from "next/headers"
+
+const getHeaders = () => ({
+	Cookie: cookies().toString(),
+});
+
+export const post = async (path: string, formData: FormData, responseCB?: (res: Response) => Promise<void>) => {
 	const res = await fetch(path, {
 		method: 'POST',
-		headers: { "Content-Type": 'application/json' },
+		headers: { "Content-Type": 'application/json', ...getHeaders() },
 		body: JSON.stringify(Object.fromEntries(formData))
 	})
+	if (responseCB) await responseCB(res)
 	const resJson = await res.json()
 	if (!res.ok) {
-		return {error: true, data: resJson} 
+		return { error: true, data: resJson }
 	}
 
-	return {error: false, data: resJson}
+	return { error: false, data: resJson }
+}
+
+export const get = async (path: string) => {
+	const res = await fetch(path, {
+		headers: { ...getHeaders() }
+	})
+	return res.json()
 }
