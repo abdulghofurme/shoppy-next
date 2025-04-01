5ae828a98183160ab5632d8989fc36d35385658c
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-04-01 14:34:49 +0700

    feat(home): Add product retrieval and display on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import getMe from "@/actions/get-me"
+import getProducts from "@/actions/get-products"
 
 export default async function Home() {
   const me = await getMe()
+  const products = await getProducts()
   return <>
     <h1>Hello {me?.email}</h1>
+
+    {products.map(product => <div key={product.id}>{product.name}</div>)}
   </>
 }
